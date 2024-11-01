f840523c08b1028ac206aec6fa686082818bdab8
FIGRIHANS <143943611+FIGRIHANS@users.noreply.github.com>
2024-11-01 18:21:52 +0700

    up

---
+++ resources/js/Layouts/HeaderLogView.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="flex flex-col">
+      <nav class="flex gap-5 justify-between items-start px-20 pt-5 pb-1 w-full bg-white max-md:px-5 max-md:max-w-full">
+        <div class="flex gap-1 items-center text-lg font-medium tracking-wide text-gray-900">
+          <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/a634d359f56ff8162e239acb358b6d270baf42ad608fc3dde1f448fc411a5260?placeholderIfAbsent=true&apiKey=a6a4bb1a991e4a4fa4c25aa0ed6e28da" alt="" class="object-contain shrink-0 self-stretch my-auto aspect-[1.05] w-[41px]" />
+          <div class="self-stretch my-auto">
+            MENDA<span class="font-bold">SKY</span>
+          </div>
+        </div>
+        <ul class="flex flex-wrap gap-10 items-start self-stretch my-auto text-base tracking-wide whitespace-nowrap text-zinc-800 max-md:max-w-full">
+          <li><a href="#" class="hover:underline">Beranda</a></li>
+          <li><a href="#" class="hover:underline">Reservasi</a></li>
+          <li><a href="#" class="hover:underline">Berita</a></li>
+          <li><a href="#" class="hover:underline">Panduan</a></li>
+          <li><a href="#" class="hover:underline">Kontak</a></li>
+        </ul>
+        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/6d62310482c1754abf28a347cbe1a822450aebafa5c5b2c85ae3e61584e4e45d?placeholderIfAbsent=true&apiKey=a6a4bb1a991e4a4fa4c25aa0ed6e28da" alt="" class="object-contain shrink-0 max-w-full aspect-[2.32] w-[102px]" />
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Header'
+  }
+  </script>
